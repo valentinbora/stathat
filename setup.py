d528a1c672dea4438fb38bc43842851868dff6a9
Valentin Bora <contact@valentinbora.com>
2013-03-26 12:04:48 +0200

    Created StatHat python client egg

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='StatHat',
+  version='1.0',
+  author='Valentin Bora',
+  author_email = 'valentin@gosimplysocial.com',
+  description = 'StatHat Python client',
+  url = "http://",
+  license = "MIT",
+  packages = find_packages(exclude=['tests']),
+  zip_safe = False,
+  include_package_data = True,
+  package_data = { 
+    '': ['*.png'], 
+  },
+  entry_points = {
+  }
+)
